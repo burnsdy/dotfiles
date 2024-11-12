da3cc30555a9b52e55e641966e18ba18ec7aaadb
Dylan Burns <dylan.burns@datadoghq.com>
2024-11-12 15:51:27 -0500

    Set lazyvim as neovim config

---
+++ lazyvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+
+map({ "n", "v" }, "J", "10j")
+map({ "n", "v" }, "K", "10k")
+map("n", "<leader>j", "J")
