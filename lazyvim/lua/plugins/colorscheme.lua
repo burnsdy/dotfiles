0966c8457c44eb9ecf8cf1c348883b2abc3a78f6
Dylan Burns <dylan.burns@datadoghq.com>
2024-11-26 17:19:48 -0500

    Add LazyVim recommended plugins, update colorscheme to nordfox, extend mini.surround and nvim-cmp plugins, add basic J/K keymaps

---
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "EdenEast/nightfox.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load color scheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordfox",
+    },
+  },
+}
