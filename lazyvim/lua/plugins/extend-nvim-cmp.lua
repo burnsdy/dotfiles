a3991803209753f75f5e2ea4141086dbd87ef21c
Dylan Burns <dylan.burns@datadoghq.com>
2024-12-04 20:53:57 -0500

    Update nvim-cmp select mapping to <Tab>

--- lazyvim/lua/plugins/extend-nvim-cmp.lua
+++ lazyvim/lua/plugins/extend-nvim-cmp.lua
@@ -6,7 +6,7 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-l>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
         ["<CR>"] = function(fallback)
           cmp.abort()
           fallback()
