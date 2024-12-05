aa14a55327562a7e0c39338aa9e731ab578cc3d2
Dylan Burns <dylan.burns@datadoghq.com>
2024-12-04 20:57:04 -0500

    Allow counts to be added to bufferline navigation commands

---
+++ lazyvim/lua/plugins/extend-bufferline.lua
@@ -0,0 +1,34 @@
+return {
+  "akinsho/bufferline.nvim",
+  -- Allow counts to added to the mappings
+  keys = {
+    {
+      "L",
+      function()
+        vim.cmd("bnext " .. vim.v.count1)
+      end,
+      desc = "Next buffer",
+    },
+    {
+      "H",
+      function()
+        vim.cmd("bprev " .. vim.v.count1)
+      end,
+      desc = "Previous buffer",
+    },
+    {
+      "]b",
+      function()
+        vim.cmd("bnext " .. vim.v.count1)
+      end,
+      desc = "Next buffer",
+    },
+    {
+      "[b",
+      function()
+        vim.cmd("bprev " .. vim.v.count1)
+      end,
+      desc = "Previous buffer",
+    },
+  },
+}
