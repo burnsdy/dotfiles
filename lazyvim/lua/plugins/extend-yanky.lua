760b956afd190e8e5f54dae79e2173cbcb7d5f93
Dylan Burns <dylan.burns@datadoghq.com>
2024-12-09 10:11:55 -0500

    Set up boilerplate file to extend yanky.nvim, next will add put keymaps for putting from clipboard

---
+++ lazyvim/lua/plugins/extend-yanky.lua
@@ -0,0 +1,9 @@
+return {
+  "gbprod/yanky.nvim",
+  opts = {
+    -- Create which-key namespace for <Space>p
+    -- Remap <Space>ph to open yanky.nvim history (<Space>p by default)
+    -- Set <Space>pp to put after from system clipboard
+    -- Set <Space>pP to put before from system clipboard
+  },
+}
