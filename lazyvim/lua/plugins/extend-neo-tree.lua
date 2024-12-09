8cf106df50b02e99c62556e0722a5977ed9a03d1
Dylan Burns <dylan.burns@datadoghq.com>
2024-12-09 09:56:12 -0500

    Update neo-tree config to show hidden files by default

---
+++ lazyvim/lua/plugins/extend-neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        -- Show hidden files by default
+        visible = true,
+      },
+    },
+  },
+}
