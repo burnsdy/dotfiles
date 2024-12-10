540d1fddcd95e87940b46f9201c8e96b1ff59d7d
Dylan Burns <dylan.burns@datadoghq.com>
2024-12-10 15:43:15 -0500

    Remap <leader>; to save buffer

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -3,8 +3,8 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
--- Remap <leader>w to save
--- map({ "n", "v" }, "<leader>w", "<cmd>write<cr>", { desc = "Save file", silent = true })
+-- Remap <leader>; to save
+map({ "n", "v" }, "<leader>;", "<cmd>write<cr>", { desc = "Save file", silent = true })
 
 -- Remap J and K for navigation
 map({ "n", "v" }, "J", "10j", { desc = "Scroll down 10 lines", silent = true, remap = true })
