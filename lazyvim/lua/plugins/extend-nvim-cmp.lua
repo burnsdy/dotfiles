6a591461fdacc05157edd2f000a88586309da62d
Dylan Burns <dylan.burns@datadoghq.com>
2024-12-16 15:22:33 -0500

    Update plugin format to use single-plugin config format wherever possible

--- lazyvim/lua/plugins/extend-nvim-cmp.lua
+++ lazyvim/lua/plugins/extend-nvim-cmp.lua
@@ -1,17 +1,15 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        ["<CR>"] = function(fallback)
-          cmp.abort()
-          fallback()
-        end,
-      })
-    end,
-  },
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+      ["<CR>"] = function(fallback)
+        cmp.abort()
+        fallback()
+      end,
+    })
+  end,
 }
