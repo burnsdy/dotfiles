200035be34a05456fba760f629359802b3510694
Dylan Burns <dylan.burns@datadoghq.com>
2024-12-16 15:19:24 -0500

    Add Neotest plugins for Jest and Vitest

---
+++ lazyvim/lua/plugins/jest.lua
@@ -0,0 +1,7 @@
+return {
+  { "nvim-neotest/neotest-jest" },
+  {
+    "nvim-neotest/neotest",
+    opts = { adapters = { "neotest-jest" } },
+  },
+}
---
+++ lazyvim/lua/plugins/vitest.lua
@@ -0,0 +1,7 @@
+return {
+  { "marilari88/neotest-vitest" },
+  {
+    "nvim-neotest/neotest",
+    opts = { adapters = { "neotest-vitest" } },
+  },
+}
