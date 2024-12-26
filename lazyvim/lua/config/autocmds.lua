bd98aaa68269548c3e9b2e6c23d6f86571c04d72
Dylan Burns <dylan.burns@datadoghq.com>
2024-12-25 22:00:31 -0500

    Add Neovim autocmd for <leader>* keymap to bold selection in Markdown files

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set <leader>* keymap to bold current line or selection in Markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown", -- Only apply to Markdown files
+  callback = function()
+    vim.keymap.set("n", "<leader>*", function()
+      vim.cmd("normal _wv$gsa*v$gsa*")
+    end, { buffer = true, desc = "Bold current line in Markdown" })
+    vim.keymap.set("v", "<leader>*", function()
+      vim.cmd("normal <esc>gvgsa*gvgsa*")
+    end, { buffer = true, desc = "Bold current line in Markdown" })
+  end,
+})
