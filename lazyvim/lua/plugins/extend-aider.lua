6fe9b6160269cc54f2e101d843767fcc2af25d4b
Dylan Burns <dylan.burns@datadoghq.com>
2025-03-17 11:24:55 -0400

    Add aider plugin (even though I don't use it)

---
+++ lazyvim/lua/plugins/extend-aider.lua
@@ -0,0 +1,8 @@
+return {
+  "joshuavial/aider.nvim",
+  opts = {
+    auto_manage_context = true, -- automatically manage buffer context
+    default_bindings = true, -- use default <leader>A keybindings
+    debug = false, -- enable debug logging
+  },
+}
